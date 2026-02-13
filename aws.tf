7da1304d525a93c15580913bbaa8912303f21902
Atul Kamble <atul_kamble@hotmail.com>
2026-02-13 08:34:59 +0530

    update

--- aws.tf
+++ aws.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_ec2_instance" "webserver" {
+resource "aws_instance" "webserver" {
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t3.medium"
 
