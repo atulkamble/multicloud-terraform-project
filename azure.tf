79259d5edf26b0889a6ae18eb6d0ef7e2fc92677
Atul Kamble <atul_kamble@hotmail.com>
2026-02-13 08:50:38 +0530

    update

--- azure.tf
+++ azure.tf
@@ -22,6 +22,37 @@ resource "azurerm_subnet" "mySubnet" {
   address_prefixes     = ["10.0.1.0/24"]        
 }
 
+resource "azurerm_public_ip" "myPublicIP" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.myRG.location
+  resource_group_name = azurerm_resource_group.myRG.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_security_group" "myNSG" {
+  name                = "myNSG"
+  location            = azurerm_resource_group.myRG.location
+  resource_group_name = azurerm_resource_group.myRG.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "myNICNSG" {
+  network_interface_id      = azurerm_network_interface.myNIC.id
+  network_security_group_id = azurerm_network_security_group.myNSG.id
+}
+
 resource "azurerm_network_interface" "myNIC" {
   name                = "myNIC"
   location            = azurerm_resource_group.myRG.location
@@ -31,6 +62,7 @@ resource "azurerm_network_interface" "myNIC" {
     name                          = "myIPConfig"
     subnet_id                     = azurerm_subnet.mySubnet.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myPublicIP.id
   }
 }
 
